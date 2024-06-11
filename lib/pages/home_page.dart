70e09149bc6ddad8a492d7407df9b87ee666b34b
Elmar Səlimov <elmarsalimov085@gmail.com>
2024-06-11 23:00:46 +0400

    test

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text("Hello Turqay!"),
+        child: Text("Hello Turqay and Elmar!"),
       ),
     );
   }
