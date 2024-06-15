d8342435fd372e1d03da9cc4d175f995a1a1de1c
Elmar Səlimov <elmarsalimov085@gmail.com>
2024-06-15 19:51:45 +0400

    Splash screen added

---
+++ lib/pages/onboarding_screen.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/components/last_onboarding_tile.dart';
+import 'package:netflix_clone/pages/home_page.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:netflix_clone/components/onboarding_tile.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  final controller = PageController();
+
+  OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      // Netflix icon and Privacy and Help tab
+      appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    GestureDetector(
+                      child: Image.asset(
+                        'lib/assets/netflix-icon.png',
+                        width: 100,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Row(
+                children: [
+                  Text("Privacy",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20)),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Text("Help",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20))
+                ],
+              )
+            ],
+          )),
+      body: Stack(
+        children: [
+          // Main Screen
+          PageView(
+            controller: controller,
+            children: const [
+              OnboardingTile(
+                  imagePath: 'lib/assets/1.jpeg',
+                  mainText: "Watch everywhere",
+                  secondText: "Stream on phone, tablet, laptop and TV."),
+              OnboardingTile(
+                  imagePath: 'lib/assets/2.jpeg',
+                  mainText: "There's a plan for every fan",
+                  secondText: "Small price. Big entertainment."),
+              OnboardingTile(
+                  imagePath: 'lib/assets/3.jpeg',
+                  mainText: "Cancel online anytime",
+                  secondText: "Join today, no reason to wait."),
+              LastOnboardingTile(
+                  imagePath: 'lib/assets/netflix-wallpaper.jpg',
+                  mainText: "How do I watch?",
+                  secondText:
+                      "Members that subscribe to Netflix can watch here in the app.")
+            ],
+          ),
+
+          // Smooth page indicator and Sign-in
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: const EdgeInsets.only(bottom: 60),
+              padding: const EdgeInsets.all(8.0),
+              child: SmoothPageIndicator(
+                controller: controller,
+                count: 4,
+                effect: const ScrollingDotsEffect(
+                  activeDotColor: Colors.red,
+                  activeDotScale: 1,
+                  dotWidth: 8,
+                  dotHeight: 8,
+                ),
+              ),
+            ),
+          ),
+
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: GestureDetector(
+              onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const HomePage())),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 255, 17, 0),
+                    borderRadius: BorderRadius.circular(4)),
+                child: const Center(
+                  child: Text("SIGN IN",
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
