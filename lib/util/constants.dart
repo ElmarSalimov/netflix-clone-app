d9d711ca2a3f7afe9a8a87a6f2bd689a30f9e997
Elmar Salimov <elmarsalimov085@gmail.com>
2024-06-29 14:55:42 +0400

    Elmar from main

---
+++ lib/util/constants.dart
@@ -0,0 +1,4 @@
+const apiKey = "bb8c312901e0da98908810ed6cc6ecad";
+const imageUrl = "https://image.tmdb.org/t/p/w500";
+const baseUrl = "https://api.themoviedb.org/3/";
+var key = "?api_key=$apiKey";
